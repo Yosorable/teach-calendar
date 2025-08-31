d292f588d18ee0a5ea29a3c2957a5d753dcde73a
Yosorable <1354464666@qq.com>
2025-08-31 14:34:53 +0800

    站点地址

--- demo-data.js
+++ demo-data.js
@@ -1,10 +1,13 @@
-"#" +
+"https://lzyable.com/c#" +
   encodeURIComponent(
     JSON.stringify({
       calendar: {
         start: "2025-09-01",
         end: "2026-02-08",
       },
+
+      // color为css颜色字符串
+      // sections的格式为 <星期-上下午-节>，都是从0开始
       course: [
         {
           name: "数学",
@@ -17,7 +20,7 @@
           name: "数学",
           className: "六(2)班",
           room: "A102",
-          sections: ["1-0-0", "1-0-2", "2-0-2", "2-1-2", "3-0-2"],
+          sections: ["0-1-2", "1-0-0", "2-0-2", "2-1-2", "3-0-2"],
           color: "hsla(29, 66%, 86%, 0.9)",
         },
         {
